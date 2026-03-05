835358587e724ae6c2fa02cc33f25fc4bdc1aece
James8234 <colinx619@gmail.com>
2026-03-04 16:31:22 -0800

    UI update for motor controls, and correct centering

--- render_ui.py
+++ render_ui.py
@@ -9,16 +9,21 @@ def render_screen(stdscr):
 	stdscr.clear()
 
 	#Defines string that fits within terminal width
-	motorControls = "Motor Controls: W Forward, S - Backwards, E - breaks"
-	help = "<- A | D ->"[:width-1]
+	motorControls = "Motor Controls: W Forward, S - Backwards, E - breaks"[:width-1]
+	servoControls = "Servo Controls: <- A | D ->"[:width-1]
 	quit = "Enter q to quit program"[:width-1]
 
 	center =  round(width / 2)
-	stdscr.addstr(1, center, "^")
-	stdscr.addstr(2, center, "|")
-	stdscr.addstr(3, center, motorControls)
-	stdscr.addstr(4, center, help)
-	stdscr.addstr(5, center, quit)
+
+	center_MC = max(round(center - (len(motorControls) / 2)), 0)
+	center_SC = max(round(center - (len(servoControls) / 2)), 0)
+	center_Q  = max(round(center - (len(quit) / 2)), 0)
+
+
+
+	stdscr.addstr(3, center_MC, motorControls)
+	stdscr.addstr(4, center_SC, servoControls)
+	stdscr.addstr(5, center_Q, quit)
 
 	#update the screen
 	stdscr.refresh()
